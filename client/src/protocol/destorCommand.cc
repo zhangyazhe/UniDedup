9ebd9f3565b72941f97abc5c31cbd7640632585b
zz7yuqi <zz7yuqi@qq.com>
2022-09-03 14:47:44 +0800

    init: destorCommand

--- client/src/protocol/destorCommand.cc
+++ client/src/protocol/destorCommand.cc
@@ -1,9 +1,62 @@
-#include "DestorCommand.hh"
+#include "destorCommand.hh"
 
 destorCommand::destorCommand(/* args */)
 {
+    _destorCmd = (char*)calloc(MAX_COMMAND_LEN, sizeof(char));
+    _cmLen = 0;
+    _rKey = "destor_request";
 }
 
 destorCommand::~destorCommand()
 {
+    if (_destorCmd)
+    {
+        free(_destorCmd);
+        _destorCmd = 0;
+    }
+    _cmLen = 0;
+}
+
+void destorCommand::writeInt(int value) {
+  int tmpv = htonl(value);
+  memcpy(_destorCmd + _cmLen, (char*)&tmpv, 4); _cmLen += 4;
+}
+
+void destorCommand::writeString(string s) {
+  int slen = s.length();
+  int tmpslen = htonl(slen);
+  // string length
+  memcpy(_destorCmd + _cmLen, (char*)&tmpslen, 4); _cmLen += 4;
+  // string
+  memcpy(_destorCmd + _cmLen, s.c_str(), slen); _cmLen += slen;
+}
+
+int destorCommand::readInt() {
+  int tmpint;
+  memcpy((char*)&tmpint, _destorCmd + _cmLen, 4); _cmLen += 4;
+  return ntohl(tmpint);
+}
+
+int destorCommand::readRawInt() {
+  int tmpint;
+  memcpy((char*)&tmpint, _destorCmd + _cmLen, 4); _cmLen += 4;
+  return tmpint;
+}
+
+std::string destorCommand::readString() {
+  std::string toret;
+  int slen = readInt();
+  char* sname = (char*)calloc(sizeof(char), slen+1);
+  memcpy(sname, _destorCmd + _cmLen, slen); _cmLen += slen;
+  toret = string(sname);
+  free(sname);
+  return toret;
+}
+
+int destorCommand::getType() {
+  return _type;
+}
+
+unsigned int destorCommand::getClientip() {
+  return _clientIp;
 }
--- client/src/protocol/destorCommand.hh
+++ client/src/protocol/destorCommand.hh
@@ -9,9 +9,29 @@ class destorCommand
 {
 private:
     /* data */
+    char *_destorCmd;
+    int _cmLen;
+    std::string _rKey;
+    int _type;
+    unsigned int _clientIp;
+
 public:
     destorCommand(/* args */);
     ~destorCommand();
+
+    // basic construction methods
+    void writeInt(int value);
+    void writeString(string s);
+    int readInt();
+    int readRawInt();
+    string readString();
+
+    int getType();
+    unsigned int getClientip();
+
+    // send method
+    void sendTo(unsigned int ip);
+    void sendTo(redisContext* sendCtx);
 };
 
 
