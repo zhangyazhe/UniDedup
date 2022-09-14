0a616f3630ad0bcba51824a449dd1ce45d2c0bb0
wl83 <451161369@qq.com>
2022-09-14 12:24:11 +0800

    echash & filerecipe

--- client/src/common/FileRecipe.hh
+++ client/src/common/FileRecipe.hh
@@ -22,6 +22,6 @@ struct fileRecipe* new_fileRecipe(const char* filename, int num);
 void delete_fileRecipe(struct fileRecipe* fr);
 struct fileRecipe* genFileRecipe(const char* filename, vector<struct group*>& gps);
 int setFileRecipe(struct fileRecipe* fr);
-struct FileRecipe* getFileRecipe(const char* filename);
+struct fileRecipe* getFileRecipe(const char* filename);
 
 #endif
