7921af9757d71f912458ff673bea193635c38a66
franklin1226 <1979933731@qq.com>
2022-09-14 15:01:43 +0800

    Finish Assembling.cc and Assembling.hh

--- client/src/common/Assembling.cc
+++ client/src/common/Assembling.cc
@@ -1,1 +1,27 @@
 #include "Assembling.hh"
+
+void assemble(const char *path) {
+    int fd = -1;
+    fd = open(path, O_WRONLY | O_CREAT, S_IRWXU);
+    if (fd < 0) {
+        fprintf(stderr, "assemble: open file failed\n");
+        return ;
+    }
+    unsigned char assembly_area[ASSEMBLE_BUFFER_LEN] = {'0'};
+    int offset = 0;
+    chunk *c = NULL;
+    while ((c = sync_queue_pop(receive_queue)) != nullptr) {
+        assert(c->size < ASSEMBLE_BUFFER_LEN * sizeof(unsigned char));
+        if (offset + c->size < ASSEMBLE_BUFFER_LEN * sizeof(unsigned char)) {
+            memcpy(assembly_area + offset, c->data, c->size);
+            offset += c->size;
+            free_chunk(c);
+            continue;
+        }
+        write(fd, assembly_area, offset);
+        offset = 0;
+        memcpy(assembly_area + offset, c->data, c->size);
+        free_chunk(c);
+    }
+    close(fd);
+}
--- client/src/common/Assembling.hh
+++ client/src/common/Assembling.hh
@@ -2,6 +2,15 @@
 #define _ASSEMBLING_HH_
 
 #include "Receiving.hh"
+#include "Chunking.hh"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Define the length of the assembly area, the default value is 30 4MB container size */
+#define ASSEMBLE_BUFFER_LEN 30 * 4194304ll
 
 void assemble(const char *path);
 
